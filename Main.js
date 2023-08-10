5b9969b6a71d82ff509f3d0c644babcb50d5fbb1
NawalMalki <malkinawal2004@gmail.com>
2023-08-10 21:27:31 +0200

    first commit

---
+++ Main.js
@@ -0,0 +1,7 @@
+const HamburgerMenu = document.querySelector(".Menu");
+const Menu = document.querySelector(".Side"); 
+
+
+HamburgerMenu.addEventListener("click", function() {
+    Menu.style.display = "block";
+});
